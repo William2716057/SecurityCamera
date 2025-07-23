0a3ea4a1fa86f490722e4f59006fba748455b346
name <willspri@gmail.com>
2025-07-23 13:21:24 +1200

    tone added

--- security.py
+++ security.py
@@ -1,4 +1,27 @@
 import cv2
+import pyaudio
+import numpy as np
+
+#tone for detection
+def playTone():
+    p = pyaudio.PyAudio()
+    volume = 0.5     # range [0.0, 1.0]
+    fs = 44100       # sampling rate, Hz
+    duration = 0.2   # seconds
+    f = 440.0        # sound frequency (Hz)
+
+    # generate sine wave tone
+    samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
+
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=fs,
+                    output=True)
+
+    stream.write(volume*samples)
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
 #initialise person detector
 hog =cv2.HOGDescriptor()
@@ -23,11 +46,21 @@ while True:
     for (x, y, w, h) in rects:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0))
         
+    #if detected = True:
+    detected = False
+    for (x, y, w, h) in rects:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        detected = True
+
+    # If any person detected, play tone
+    if detected:
+        playTone()
+
     cv2.imshow("Figure detect", frame)
-        
+
     key = cv2.waitKey(1)
-    if key == 27:
+    if key == 27:  # ESC key
         break
-    
+
 cap.release()
 cv2.destroyAllWindows()
